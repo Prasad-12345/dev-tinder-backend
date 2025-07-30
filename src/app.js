dd65ed7654ef273f311024fd31b3e213e5f30ff3
Prasad-12345 <prasadsomvanshi471@gmail.com>
2025-07-30 22:35:26 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,13 +2,31 @@ const express = require("express")
 
 const app = express()
 
-app.use('/', (req, res) => {
-    res.send("Hello World ....")
-})
+// app.use('/', (req, res) => {
+//     res.send("Hello World ....")
+// })
 
-app.use('/test', (req, res) => {
-    res.send("this is testing")
-})
+app.use('/user', 
+    [
+        (req, res, next) => {
+            console.log('1st response')
+            next()
+        },
+        (req, res, next) => {
+            console.log('2nd response')
+            next()
+        },
+        (req, res, next) => {
+            console.log('3rd response')
+            next()
+        },
+        (req, res, next) => {
+            console.log('4th response')
+            // next()
+            res.send("4th response")
+        }
+    ]
+)
 
 app.listen("7777", () => {
     console.log("server is running on a port 7777")
