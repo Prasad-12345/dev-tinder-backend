e81503fbe7aba72c013d04c183b152fa6cb2fc19
Prasad-12345 <prasadsomvanshi471@gmail.com>
2025-09-20 16:18:22 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(express.json())
 
 app.use((req, res, next) => {
   if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Origin", allowedOrigin);
+    res.header("Access-Control-Allow-Origin", "https://dev-tinder-web-2c4g.onrender.com");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
